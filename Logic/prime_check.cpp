710cdeeb991e77c801734a197b8ab04d182b25d8
Gandharv Nagrani <84096769+gandharvnagrani@users.noreply.github.com>
2022-01-19 23:45:32 +0300

    code

---
+++ Logic/prime_check.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        for(int j=2; j<n; j++){
+            if(n%j==0){
+                cout<<"Composite"<<endl;
+                }
+        else{
+            cout<<"Prime"<<endl;
+                }
+                
+            }
+                
+            }
+        }
+    
